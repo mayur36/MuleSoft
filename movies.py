3f77879dcb05d0dc77cc11b86c16522988398431
mayur36 <77525201+mayur36@users.noreply.github.com>
2022-02-10 00:49:38 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,22 +1,29 @@
 
 import sqlite3
-
-
 cnt = sqlite3.connect("bollywood")
-
-
 cnt.execute('''CREATE TABLE movies(
 name TEXT,
 actor TEXT,
 actress TEXT,
 yearOfRelease INTEGER,);''')
-
 cnt.execute('''INSERT INTO movies VALUES(
 '3 idiots', 'Amir Khan', 'Kareena Kapur', 2009);''')
-
 cnt.commit()
-
-
 cnt.execute('SELECT * FROM movies')
-
 cnt.execute('SELECT * FROM movies WHERE actress = 'Kareena Kapur'')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
